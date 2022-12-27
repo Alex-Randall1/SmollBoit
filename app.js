f60600494364aefd29897e859f818da3977a1fbd
Alex-Randall <51383341+Alex-Randall1@users.noreply.github.com>
2022-12-26 21:05:29 -0600

    Add files via upload
    Added basic commands

---
+++ app.js
@@ -0,0 +1,53 @@
+const tmi = require('tmi.js');
+
+const client = new tmi.Client({
+	options: { debug: true },
+	identity: {
+		username: 'smollbotpenguin',
+		password: '',
+	},
+	channels: [ 'smollboipenguin' ]
+});
+
+client.connect();
+
+
+
+//commands
+
+client.on('message', (channel, tags, message, self) => {
+	// Ignore echoed messages.
+	if(self) return;
+
+	if(message.toLowerCase() === '!hello') {
+		// "@username, heya!"
+		client.say(channel, `@${tags.username}, whats up nerd!`);
+	}
+
+  if(message.toLowerCase() === '!age') {
+    //age command
+    client.say(channel, `Nobody cares about your age sit down.`);
+  }
+
+	if(message.toLowerCase() === '!project') {
+    //age command
+    client.say(channel, `Ender City, cyber punk style city. Image shows inspiration for the build. Later in the build will come up with custom point redeems.`);
+  }
+
+	if(message.toLowerCase() === '!break') {
+    //age command
+    client.say(channel, `Going on a little break to refresh be back soon. Shortstack or Waddly is in charge until I get back!`);
+  }
+
+	if(message.toLowerCase() === '!vibe') {
+    //age command
+    client.say(channel, `wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance  wooperJam  blobDance `);
+  }
+
+	// if(message.toLowerCase() === '!age') {
+  //   //age command
+  //   client.say(channel, `Nobody cares about your age sit down.`);
+  // }
+
+
+});
